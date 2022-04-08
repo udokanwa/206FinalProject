b81c73967d66f133e4a361f4b7bfb9fce168314b
udoka <udoka@umich.edu>
2022-04-08 16:37:09 -0400

    "Starter code"

--- FlightPrices.py
+++ FlightPrices.py
@@ -1,1 +1,25 @@
+from unicodedata import category
+import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
+import os
+import sqlite3
+import unittest
 
+
+# this function will gather data from hopper website using 
+def scrape_website(url):
+
+# this function will make the connector and cursor to naviagte our database
+def create_connector_cursor():
+
+
+# using data to create database
+def create_database(data):
+
+
+# this function will create all of our final visualizations using matplotlib
+def generate_graphs():
+
+
+# call all of our functions here
+def main():
