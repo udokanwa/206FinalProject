0564c62feb41175d76a1c0a939e556d464991f85
udoka <udoka@umich.edu>
2022-04-17 13:53:59 -0400

    Finished collecting players and putting into dict

--- NBA_AllStars.py
+++ NBA_AllStars.py
@@ -8,7 +8,7 @@ import sqlite3
 import unittest
 import json
 import requests
-import re
+import unidecode
 
 # Reasearch Question: How does the number of All Stars on an NBA team affect the team's season performace?
 
@@ -26,17 +26,32 @@ def find_all_AllStars():
         if player == None or tr.find('td').get("bgcolor") != "#CFECEC":
             continue
         else:
-            name = player.find('a').getText()
+            name = unidecode.unidecode(player.find('a').getText())
             player_list.append(name)
-    print(player_list)
-    print(len(player_list))
+    # To account for 2 players who are currently inactive in the 21-22 season, Marc Gasol and Jeff Teague
+    player_list.remove("Marc Gasol")
+    player_list.remove("Jeff Teague")
+    # 69 Active All-Stars
     return player_list
 
 # returns a dictionary with the number of AllStars on each NBA team (if there's more than 0)
 def count_AllStarTeams(player_list):
-    url = "https://basketball.realgm.com/nba/players"
+    url = r"https://basketball.realgm.com/nba/players"
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
+    # tag = soup.find_all('table')[1]
+    team_dict = {}
+    table = soup.find('tbody')
+    for tr in table.find_all('tr'):
+        player = tr.find_all('a')[0].getText()
+        if player in player_list:
+            team = tr.find_all('a')[1].getText()
+            if team not in team_dict:
+                team_dict[team] = 1
+            else:
+                team_dict[team] += 1
+    print(team_dict)
+    return team_dict
 
 # this function will gather data about each team from the api  
 def scrape_api(url):
@@ -53,7 +68,7 @@ def create_connector_cursor(db_filename):
 This function will use the collection data to create database with each team, their number of AllStars, 
 and their season statistics. This database should be organized by number of All Stars on each team.
 '''
-def create_database(data):
+def create_database(data, cur, conn):
     #info = json.loads(data)
     pass
 
@@ -66,7 +81,7 @@ def generate_graphs():
 # call all of our functions here
 def main():
     AllStarList = find_all_AllStars()
-    TeamDict = count_allStarNum(AllStarList)
+    TeamDict = count_AllStarTeams(AllStarList)
 
 if __name__ == '__main__':
     main()
