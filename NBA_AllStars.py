b908acbb36d3299cd3d75d0b73770f774db3cd36
udoka <udoka@umich.edu>
2022-04-18 15:45:02 -0400

    turned allstar into python file

--- NBA_AllStars.py
+++ NBA_AllStars.py
@@ -1,7 +1,6 @@
 from turtle import bgcolor
 from unicodedata import category
 from unittest.mock import NonCallableMagicMock
-import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
 import os
 import sqlite3
@@ -10,9 +9,9 @@ import json
 import requests
 import unidecode
 
-# Reasearch Question: How does the number of All Stars on an NBA team affect the team's season performace?
+# Reasearch Question: How does the number of All Stars on an NBA team affect the team's regular season performace?
 
-# this function will find all of the current active NBA All Stars in the NBA
+# This function will find all of the current active NBA All Stars in the NBA
 
 def find_all_AllStars():
     url = r"https://en.wikipedia.org/wiki/List_of_NBA_All-Stars"
@@ -28,18 +27,17 @@ def find_all_AllStars():
         else:
             name = unidecode.unidecode(player.find('a').getText())
             player_list.append(name)
-    # To account for 2 players who are currently inactive in the 21-22 season, Marc Gasol and Jeff Teague
+    # To account for 2 players who are still in the NBA but inactive in the 21-22 season, Marc Gasol and Jeff Teague
     player_list.remove("Marc Gasol")
     player_list.remove("Jeff Teague")
     # 69 Active All-Stars
     return player_list
 
-# returns a dictionary with the number of AllStars on each NBA team (if there's more than 0)
+# returns a dictionary with the number of All Stars on each NBA team (if there's more than 0)
 def count_AllStarTeams(player_list):
     url = r"https://basketball.realgm.com/nba/players"
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
-    # tag = soup.find_all('table')[1]
     team_dict = {}
     table = soup.find('tbody')
     for tr in table.find_all('tr'):
@@ -55,7 +53,19 @@ def count_AllStarTeams(player_list):
 
 # this function will gather data about each team from the api  
 def scrape_api(url):
-    pass
+    url = "https://api-nba-v1.p.rapidapi.com/standings"
+
+    querystring = {"league":"standard","season":"2022"}
+
+    headers = {
+	"X-RapidAPI-Host": "api-nba-v1.p.rapidapi.com",
+	"X-RapidAPI-Key": "SIGN-UP-FOR-KEY"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    print(response.text)
+    return(response.text)
     
 # this function will make the connector and cursor to naviagte our database and return them as well
 def create_connector_cursor(db_filename):
@@ -68,13 +78,9 @@ def create_connector_cursor(db_filename):
 This function will use the collection data to create database with each team, their number of AllStars, 
 and their season statistics. This database should be organized by number of All Stars on each team.
 '''
-def create_database(data, cur, conn):
+def create_database(data, team_dict, cur, conn):
     #info = json.loads(data)
-    pass
 
-
-# this function will create all of our final visualizations using matplotlib
-def generate_graphs():
     pass
 
 
@@ -82,6 +88,8 @@ def generate_graphs():
 def main():
     AllStarList = find_all_AllStars()
     TeamDict = count_AllStarTeams(AllStarList)
+    data = scrape_api()
+    create_database(data, TeamDict, cur, conn)
 
 if __name__ == '__main__':
     main()
